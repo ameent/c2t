a64bdcc72759294ea115dc1ea0a769fe70268777
Ameen Tayyebi <ameen.tayyebi@gmail.com>
2014-03-05 18:31:27 -0500

    Generation of class names and public methods is functional. First iteration, some parameter names are incorrect and inheritance is not observed

--- generator.py
+++ generator.py
@@ -5,12 +5,12 @@ class Generator:
 
     output_file_path = ""
     module_name = ""
-    preprocessed_headers = []
+    preprocessed_classes = []
 
-    def __init__(self, preprocessed_headers, output_file_path, module_name):
+    def __init__(self, preprocessed_classes, output_file_path, module_name):
         self.output_file_path = output_file_path
         self.module_name = module_name
-        self.preprocessed_headers = preprocessed_headers
+        self.preprocessed_classes = preprocessed_classes
 
     def write(self):
 
@@ -25,14 +25,34 @@ class Generator:
     def write_module(self, output_file):
 
         # Write module declaration
-        if self.module_name != "":
-            output_file.write("declare module " + self.module_name + "{\r\n")
+        if self.module_name != '':
+            output_file.write('declare module %s {\r\n' % self.module_name)
 
         # Process all classes
-        self.write_preprocessed_classes(output_file)
+        for cls in self.preprocessed_classes:
+            self.write_class(cls, output_file)
 
         if self.module_name != "":
             output_file.write("}\r\n")
 
-    def write_preprocessed_classes(self, output_file):
-        pass
+    def write_class(self, cls, output_file):
+
+        output_file.write('\texport class %s {\r\n' % cls['name'])
+
+        # Generate methods
+        for method in cls['methods']['public']:
+
+            # Ignore destructors
+            if method['destructor']:
+                continue
+
+            # Generate list of parameters
+            param_list = ','.join('%s:%s' % (arg['name'], arg['type']) for arg in method['parameters'])
+
+            # Print composed signature
+            if method['constructor']:
+                output_file.write('\t\tconstructor(%s);\r\n' % param_list)
+            else:
+                output_file.write('\t\tpublic %s(%s):%s;\r\n' % (method['name'], param_list, method['rtnType']))
+
+        output_file.write('\t}\r\n')
--- preprocessor.py
+++ preprocessor.py
@@ -5,44 +5,92 @@ import CppHeaderParser
 
 class Preprocessor:
 
-    cppheaders = []
+    def __init__(self):
+        pass
+
+    headers = []
 
     def add_header(self, cppheader):
         if not isinstance(cppheader, CppHeaderParser.CppHeader):
             raise "Must provide a CppHeader"
 
-        self.cppheaders.append(cppheader)
+        self.headers.append(cppheader)
 
-    def preprocess(self, classes, parents, name_def_pairs):
-        if len(self.cppheaders) == 0:
+    def preprocess(self):
+        if len(self.headers) == 0:
             raise "No headers have been added to the preprocessor"
 
         processed_classes = []
 
-        for cppheader in self.cppheaders:
-            processed_classes = processed_classes + self.preprocess_header(cppheader)
+        for header in self.headers:
+            processed_classes = processed_classes + self.preprocess_header(header)
 
         return processed_classes
 
     @staticmethod
-    def preprocess_header(cppheader):
+    def preprocess_header(header):
 
         processed_classes = []
 
-        for classname, clazz in cppheader.classes.items() + cppheader.structs.items():
-            pass
+        for cls in header.classes.values():
+            if Preprocessor.preprocess_class(cls):
+                processed_classes.append(cls)
 
         return processed_classes
 
     @staticmethod
+    def preprocess_class(cls):
+        # If the class does not have a constructor, then we won't be able to construct one
+        # in JS world either, so we ignore classes without constructors
+        if not Preprocessor.has_constructor(cls):
+            return False
+
+        # Change :: to . to match Typescript syntax
+        cls['name'] = cls['name'].replace('::', '.')
+
+        # Go over all the methods that are public and change their parameter types to Typescript syntax
+        for method in cls['methods']['public']:
+
+            # Fix return type of method
+            method['rtnType'] = Preprocessor.swap_builtin_types(Preprocessor.clean_type(method['rtnType']))
+
+            # For each of the parameters, fix the types
+            for arg in method['parameters']:
+                arg['type'] = Preprocessor.swap_builtin_types(Preprocessor.clean_type(arg['type']))
+
+        # Fix type of public properties
+        for property in cls['properties']['public']:
+            property['type'] = Preprocessor.swap_builtin_types(Preprocessor.clean_type(property['type']))
+
+        # Inheritance chain
+        for parent in cls['inherits']:
+            parent['class'] = parent['class'].replace('::', '.')
+
+        # Recurse on nested classes
+        for nested_cls in cls['nested_classes']:
+            Preprocessor.preprocess_class(nested_cls)
+
+        return True
+
+
+    @staticmethod
     def has_constructor(cls):
-        for method in cls['methods']:
-            if method['constructor'] and not method['destructor']: return True
+        for method in cls['methods']['public']:
+            if method['constructor'] and not method['destructor']:
+                return True
+
         return False
 
     @staticmethod
+    def swap_builtin_types(t):
+        return t.replace('int', 'number').replace('float', 'number').replace('double', 'number')\
+            .replace('bool', 'boolean')
+
+    @staticmethod
     def clean_type(t):
-        return t.replace('const ', '').replace('struct ', '').replace('&', '').replace('*', '').replace(' ', '')
+        return t.replace('inline', '').replace('const ', '').replace('struct ', '')\
+            .replace('&', '').replace('*', '')\
+            .replace(' ', '').replace('::', '.')
 
     @staticmethod
     def fix_template_value(t):  # Not sure why this is needed, might be a bug in CppHeaderParser
