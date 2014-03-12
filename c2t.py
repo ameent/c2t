6ff44cef3c7afc7f712378d0189f438cd71db776
Ameen Tayyebi <ameen.tayyebi@gmail.com>
2014-03-11 20:05:33 -0400

    Final edits before release. Asking for input parameters from the user

--- c2t.py
+++ c2t.py
@@ -3,19 +3,17 @@ __author__ = 'Ameen Tayyebi'
 from translator import *
 
 # Ask for user input
-#output_file_name = raw_input("Enter name of output file (e.g. ammo.d.ts): ")
-#header_folder = raw_input("Enter top level folder location of header files: ")
-#module_name = raw_input("Enter an optional name for top level module for generated classes: (leave empty for none) ")
-#excluded_folders = Translator.grab_folder_exclusions()
+output_file_name = raw_input("Enter name of output file (e.g. ammo.d.ts): ")
+header_folder = raw_input("Enter top level folder location of header files: ")
+module_name = raw_input("Enter a name for top level module for generated classes: ")
+excluded_folders = Translator.grab_folder_exclusions()
 
-output_file_name = "ammo.d.ts"
-header_folder = "tests"
-module_name = "Ammo"
-excluded_folders = []
+if module_name == '' or header_folder == '' or output_file_name == '':
+    print 'You need to specify a module name, output file name and location of header files.'
+else:
+    # Create a translator class to perform the actual parsing
+    translator = Translator(output_file_name, header_folder, module_name, excluded_folders)
 
-# Create a translator class to perform the actual parsing
-translator = Translator(output_file_name, header_folder, module_name, excluded_folders)
-
-translator.parse()
-translator.preprocess()
-translator.dump()
+    translator.parse()
+    translator.preprocess()
+    translator.dump()
